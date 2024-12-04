31e24cf150a2389f3a0a22e10bfacf171015048b
Raphael Cunha <raphaelsrcunha@gmail.com>
2024-12-04 16:27:13 -0500

    implements some tree's methods

--- src/main/java/com/rsc/dsa/dynamicStructures/tree/Tree.java
+++ src/main/java/com/rsc/dsa/dynamicStructures/tree/Tree.java
@@ -22,7 +22,7 @@ public class Tree<T extends Comparable> {
                         current.setLeft(newNode);
                         break;
                     }
-                } else {
+                } else { // equal or higher to the right
                     if(current.getRight() != null) {
                         current = current.getRight();
                     } else {
@@ -32,6 +32,98 @@ public class Tree<T extends Comparable> {
                 }
             }
         }
+    }
+
+    public boolean remove(T element) {
+
+        Node<T> current = this.root;
+        Node<T> fatherOfCurrent = new Node<T>(null);
+
+        while(element != null) {
+            if(current.getElement().equals(element)) {
+                break;
+            } else if(element.compareTo(current.getElement()) == -1) {
+                fatherOfCurrent = current;
+                current = current.getLeft();
+            } else {
+                fatherOfCurrent = current;
+                current = current.getRight();
+            }
+        }
+
+        if(current != null) {
+
+            //Element has two children
+            if(current.getRight() != null) {
+
+                Node<T> substitute = current.getRight();
+                Node<T> fatherOfSubstitute = current;
+                while(substitute.getLeft() != null) {
+                    fatherOfSubstitute = substitute;
+                    substitute = substitute.getLeft();
+                }
+                if(current.getElement().compareTo(fatherOfCurrent.getElement()) == -1) {
+                    fatherOfCurrent.setLeft(substitute);
+                } else {
+                    fatherOfCurrent.setRight(substitute);
+                }
+
+            } else if(current.getLeft() != null) { // children on left
+                //esquerda e tudo à direita, até que não tenha próximo. esse é o elemento que deve substituir
+                Node<T> substitute = current.getLeft();
+                Node<T> fatherOfSubstitute = current;
+                while(substitute.getRight() != null) {
+                    fatherOfSubstitute = substitute;
+                    substitute = substitute.getRight();
+                }
+                if(current.getElement().compareTo(fatherOfCurrent.getElement()) == -1) {
+                    fatherOfCurrent.setLeft(substitute);
+                } else {
+                    fatherOfCurrent.setRight(substitute);
+                }
+            } else if(current.getRight() != null) { // children on right
+
+            } else { // no children
+                if(current.getElement().compareTo(fatherOfCurrent.getElement()) == -1) {
+                    fatherOfCurrent.setLeft(null);
+                } else {
+                    fatherOfCurrent.setRight(null);
+                }
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void printInOrder(Node<T> current) {
+        if (current != null) {
+            printInOrder(current.getLeft());
+            System.out.println(current.getElement());
+            printInOrder(current.getRight());
+        }
+    }
+
+    public void printPreOrder(Node<T> current) {
+        if (current != null) {
+            System.out.println(current.getElement());
+            printPreOrder(current.getLeft());
+            printPreOrder(current.getRight());
+        }
+    }
+
+    public void printPostOrder(Node<T> current) {
+        if (current != null) {
+            printPostOrder(current.getLeft());
+            printPostOrder(current.getRight());
+            System.out.println(current.getElement());
+         }
+    }
+
+    public Node<T> getRoot() {
+        return root;
     }
 
     @Override
--- src/main/java/com/rsc/dsa/dynamicStructures/tree/test/TreeTest.java
+++ src/main/java/com/rsc/dsa/dynamicStructures/tree/test/TreeTest.java
@@ -6,13 +6,23 @@ public class TreeTest {
     public static void main(String[] args) {
         Tree t1 = new Tree<Integer>();
         t1.add(5);
-        System.out.println(t1);
         t1.add(3);
         t1.add(6);
-        System.out.println(t1);
         t1.add(4);
         t1.add(8);
         t1.add(7);
-        System.out.println(t1);
+        t1.add(35);
+        t1.add(23);
+        t1.add(99);
+        t1.add(-5);
+        t1.remove(35);
+        t1.remove(-5);
+        t1.remove(8);
+        System.out.println("\nPrinting in order");
+        t1.printInOrder(t1.getRoot());
+        //System.out.println("\nPrinting pre-order");
+        //t1.printPreOrder(t1.getRoot());
+        //System.out.println("\nPrinting post-order");
+        //t1.printPostOrder(t1.getRoot());
     }
 }
